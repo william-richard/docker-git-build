0badef285632b6dfefd125c3eaa732ecec75219d
Will Richard <will@tumblr.com>
2015-10-11 16:42:10 -0400

    Added repository wrapper, and made it configure git repo correctly

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	//	"time"
+)
+
+type Config struct {
+	work_dir_path string
+}
+
+func main() {
+	fmt.Println("Hello")
+	repo, err := NewRepository("/tmp/whatever/hello", "git@github.com:Primer42/test.git")
+	if err != nil {
+		fmt.Println("Got error:", err.Error())
+		return
+	}
+	fmt.Println(repo)
+	fmt.Println("World")
+	/*
+		watcher_delay, err := time.ParseDuration("15s")
+		if err != nil {
+			fmt.Sprintln("Error parsing delay: %s", err.Error())
+		}
+		gw := GitWatcher{"remote", watcher_delay}
+		err = gw.Watch()
+	*/
+
+}
---
+++ repository.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/libgit2/git2go"
+)
+
+type Repository struct {
+	git_repo git.Repository
+	//	watcher  Watcher
+}
+
+func NewRepository(clone_path string, remote_url string) (Repository, error) {
+	var myrepo Repository
+	fmt.Println("Got into NewRepository")
+	// ensure the parent of the clone_path exists
+	clone_path = filepath.Clean(clone_path)
+	// MkdirAll will do nothing if the dir already exists
+	os.MkdirAll(clone_path, 0755)
+	fmt.Println("Made parent. Cloning repo")
+	// Ensure the repo has been initialized
+	git_repo, err := git.InitRepository(clone_path, false)
+	if err != nil {
+		return myrepo, fmt.Errorf("Error initializing repo:", err.Error())
+	}
+	fmt.Println("initialized repo")
+
+	fmt.Println("Starting to add remote")
+	// ensure the remote on the repo matches the requested remote
+	// TODO add support for multiple remotes. Does that mean multiple watchers?
+	// remove all the existing remotes
+	remotes, err := git_repo.Remotes.List()
+	if err != nil {
+		return myrepo, fmt.Errorf("Error listing remotes:", err.Error())
+	}
+	for _, remote_name := range remotes {
+		err = git_repo.Remotes.Delete(remote_name)
+		if err != nil {
+			return myrepo, fmt.Errorf("Error deleting remote", remote_name, ":", err.Error())
+		}
+	}
+	// add the one we want
+	git_repo.Remotes.Create("origin", remote_url)
+
+	myrepo = Repository{*git_repo}
+	return myrepo, nil
+}
